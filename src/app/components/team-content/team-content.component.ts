987ff601bc316aa4f4f4a2276dde6d7d734721a9
Eduardo M. C <eduardomarquescardozo@gmail.com>
2023-05-22 13:17:27 -0300

    #16 more impactful return to user when searching

--- src/app/components/team-content/team-content.component.ts
+++ src/app/components/team-content/team-content.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { SharingService } from 'src/app/services/home-service';
 
 @Component({
@@ -6,12 +7,23 @@ import { SharingService } from 'src/app/services/home-service';
   templateUrl: './team-content.component.html',
   styleUrls: ['./team-content.component.scss']
 })
-export class TeamContentComponent  {
+export class TeamContentComponent implements OnInit, OnDestroy  {
+  subscription?: Subscription;
 
   currentTab: 0 | 1 | 2 | 3 | 4 = 0 ;
 
   constructor(private sharingService:SharingService,) {}
 
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.sharingService.getStatisticsData().subscribe(statistic => {
+      this.changeTab(1);
+    })
+  }
+  
   changeTab(tab:  0 | 1 | 2 | 3 | 4){
     let data;
     this.sharingService.getStatisticsData().subscribe(statistic => {
